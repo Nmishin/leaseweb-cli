41617a7d526769f7eed4dab81af8e3378c25a401
Nikolai Mishin <sanduku.default@gmail.com>
2025-05-23 23:58:41 +0200

    Switch version to using goreleaser

--- cmd/version.go
+++ cmd/version.go
@@ -2,41 +2,19 @@ package cmd
 
 import (
 	"fmt"
-	"runtime/debug"
 
 	"github.com/spf13/cobra"
 )
 
+var cliVersion = "dev"
+
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Show version information",
-	Long:  "Display the version and build information for leaseweb-cli",
+	Short: "Show CLI version information",
+	Long:  "Display the version information for leaseweb-cli",
 	Run: func(cmd *cobra.Command, args []string) {
-		printVersion()
+		fmt.Printf("leaseweb-cli version %s\n", cliVersion)
 	},
-}
-
-func printVersion() {
-	info, ok := debug.ReadBuildInfo()
-	if !ok {
-		fmt.Println("Version information not available")
-		return
-	}
-
-	version := info.Main.Version
-	if version == "(devel)" || version == "" {
-		version = "development"
-	}
-
-	fmt.Printf("leaseweb-cli version %s\n", version)
-
-	fmt.Printf("Go version: %s\n", info.GoVersion)
-
-	for _, setting := range info.Settings {
-		if setting.Key == "vcs.revision" {
-			fmt.Printf("Git commit: %s\n", setting.Value[:8])
-		}
-	}
 }
 
 func init() {
